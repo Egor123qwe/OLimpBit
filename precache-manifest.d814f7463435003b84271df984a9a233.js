728ffd93778d6e6b0f803c244f43f7b19e69a88f
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-10-21 03:48:45 +0300

    Updates

---
+++ precache-manifest.d814f7463435003b84271df984a9a233.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d94438f58b5f4f5f46a",
+    "url": "/OLimpBit/static/css/main.754a0bcc.chunk.css"
+  },
+  {
+    "revision": "9d94438f58b5f4f5f46a",
+    "url": "/OLimpBit/static/js/main.9d94438f.chunk.js"
+  },
+  {
+    "revision": "e99a63163aa1794106dd",
+    "url": "/OLimpBit/static/js/1.e99a6316.chunk.js"
+  },
+  {
+    "revision": "97fd4b95f43aee115131",
+    "url": "/OLimpBit/static/js/runtime~main.97fd4b95.js"
+  },
+  {
+    "revision": "05cc24702371c103845c031ff4d24c3b",
+    "url": "/OLimpBit/static/media/DirBack.05cc2470.png"
+  },
+  {
+    "revision": "db8ed3a9bffebf0e422f59580894f149",
+    "url": "/OLimpBit/static/media/SwitchOffButton.db8ed3a9.svg"
+  },
+  {
+    "revision": "bbf99ab7ff61a0abc5f59729a69559bd",
+    "url": "/OLimpBit/static/media/CollapseButton.bbf99ab7.svg"
+  },
+  {
+    "revision": "183ea5b66b5ecf2dc42c0ff76890cb35",
+    "url": "/OLimpBit/static/media/UslessButton.183ea5b6.svg"
+  },
+  {
+    "revision": "72d13424d99921a3af67d8a29da53d73",
+    "url": "/OLimpBit/static/media/ExpandButton.72d13424.svg"
+  },
+  {
+    "revision": "2b38b31a60bd6636efeb6e416c0a95ee",
+    "url": "/OLimpBit/static/media/MainStyle.2b38b31a.ttf"
+  },
+  {
+    "revision": "15d68ae923195ef48f777555a265a70d",
+    "url": "/OLimpBit/static/media/err.15d68ae9.svg"
+  },
+  {
+    "revision": "e269fccd0a139e3b8f4a2e0ad49428fd",
+    "url": "/OLimpBit/static/media/Minesweeper.e269fccd.svg"
+  },
+  {
+    "revision": "be23774f1f82ed2d8e9f01c4de9c3f67",
+    "url": "/OLimpBit/static/media/Helpbook.be23774f.svg"
+  },
+  {
+    "revision": "463b8d86968115ac66691cce2e68e64f",
+    "url": "/OLimpBit/static/media/ProgramFolder.463b8d86.svg"
+  },
+  {
+    "revision": "c2057a3149a46b311b72989c74a6fac8",
+    "url": "/OLimpBit/static/media/Search.c2057a31.svg"
+  },
+  {
+    "revision": "594821cba6e1d6ee8bb8d3e875172a4a",
+    "url": "/OLimpBit/static/media/Settings.594821cb.svg"
+  },
+  {
+    "revision": "6da867f9ef4bdb803d045a7a55c89f44",
+    "url": "/OLimpBit/static/media/Turn.6da867f9.svg"
+  },
+  {
+    "revision": "7150cccc607b3926a70a9d5885751358",
+    "url": "/OLimpBit/static/media/TurnOff.7150cccc.svg"
+  },
+  {
+    "revision": "32d1affb8e2eca48e62519c1613e45e1",
+    "url": "/OLimpBit/static/media/Calculator.32d1affb.svg"
+  },
+  {
+    "revision": "1ed889b56eef11ebd967b19b6711490f",
+    "url": "/OLimpBit/static/media/Notepad.1ed889b5.svg"
+  },
+  {
+    "revision": "d387500af507247a7dc39426c1350246",
+    "url": "/OLimpBit/static/media/Paint.d387500a.svg"
+  },
+  {
+    "revision": "1eb25095af98d8f08435544a442cf295",
+    "url": "/OLimpBit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BIT-CUP-OlimpSovet/precache-manifest.efb9cf23d6e5c45988bc96a1ed2b467d.js"
+  "/OLimpBit/precache-manifest.d814f7463435003b84271df984a9a233.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/BIT-CUP-OlimpSovet/index.html", {
+workbox.routing.registerNavigationRoute("/OLimpBit/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
