ab41dde7805af02942b1e0bcdd93b51b4157e589
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-10-21 03:44:53 +0300

    Updates

---
+++ precache-manifest.efb9cf23d6e5c45988bc96a1ed2b467d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc636800865a3395d263",
+    "url": "/BIT-CUP-OlimpSovet/static/css/main.83591cec.chunk.css"
+  },
+  {
+    "revision": "cc636800865a3395d263",
+    "url": "/BIT-CUP-OlimpSovet/static/js/main.cc636800.chunk.js"
+  },
+  {
+    "revision": "e99a63163aa1794106dd",
+    "url": "/BIT-CUP-OlimpSovet/static/js/1.e99a6316.chunk.js"
+  },
+  {
+    "revision": "39767690d3b163a8bbb4",
+    "url": "/BIT-CUP-OlimpSovet/static/js/runtime~main.39767690.js"
+  },
+  {
+    "revision": "05cc24702371c103845c031ff4d24c3b",
+    "url": "/BIT-CUP-OlimpSovet/static/media/DirBack.05cc2470.png"
+  },
+  {
+    "revision": "bbf99ab7ff61a0abc5f59729a69559bd",
+    "url": "/BIT-CUP-OlimpSovet/static/media/CollapseButton.bbf99ab7.svg"
+  },
+  {
+    "revision": "183ea5b66b5ecf2dc42c0ff76890cb35",
+    "url": "/BIT-CUP-OlimpSovet/static/media/UslessButton.183ea5b6.svg"
+  },
+  {
+    "revision": "db8ed3a9bffebf0e422f59580894f149",
+    "url": "/BIT-CUP-OlimpSovet/static/media/SwitchOffButton.db8ed3a9.svg"
+  },
+  {
+    "revision": "72d13424d99921a3af67d8a29da53d73",
+    "url": "/BIT-CUP-OlimpSovet/static/media/ExpandButton.72d13424.svg"
+  },
+  {
+    "revision": "2b38b31a60bd6636efeb6e416c0a95ee",
+    "url": "/BIT-CUP-OlimpSovet/static/media/MainStyle.2b38b31a.ttf"
+  },
+  {
+    "revision": "15d68ae923195ef48f777555a265a70d",
+    "url": "/BIT-CUP-OlimpSovet/static/media/err.15d68ae9.svg"
+  },
+  {
+    "revision": "e269fccd0a139e3b8f4a2e0ad49428fd",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Minesweeper.e269fccd.svg"
+  },
+  {
+    "revision": "be23774f1f82ed2d8e9f01c4de9c3f67",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Helpbook.be23774f.svg"
+  },
+  {
+    "revision": "463b8d86968115ac66691cce2e68e64f",
+    "url": "/BIT-CUP-OlimpSovet/static/media/ProgramFolder.463b8d86.svg"
+  },
+  {
+    "revision": "c2057a3149a46b311b72989c74a6fac8",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Search.c2057a31.svg"
+  },
+  {
+    "revision": "594821cba6e1d6ee8bb8d3e875172a4a",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Settings.594821cb.svg"
+  },
+  {
+    "revision": "6da867f9ef4bdb803d045a7a55c89f44",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Turn.6da867f9.svg"
+  },
+  {
+    "revision": "7150cccc607b3926a70a9d5885751358",
+    "url": "/BIT-CUP-OlimpSovet/static/media/TurnOff.7150cccc.svg"
+  },
+  {
+    "revision": "32d1affb8e2eca48e62519c1613e45e1",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Calculator.32d1affb.svg"
+  },
+  {
+    "revision": "1ed889b56eef11ebd967b19b6711490f",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Notepad.1ed889b5.svg"
+  },
+  {
+    "revision": "d387500af507247a7dc39426c1350246",
+    "url": "/BIT-CUP-OlimpSovet/static/media/Paint.d387500a.svg"
+  },
+  {
+    "revision": "3e668238ff490fb5f5e20daf4e5f4c6e",
+    "url": "/BIT-CUP-OlimpSovet/index.html"
+  }
+];
